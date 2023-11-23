5724ff4bd1d157936a8a9e65b50129f5e903f20f
DVdroid <anandin02@gmail.com>
2023-11-23 08:48:05 +0530

    Initial Commit

---
+++ StickyHeaderWithTabs/StickyHeaderWithTabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickyHeaderWithTabsApp.swift
+//  StickyHeaderWithTabs
+//
+//  Created by Vikash Anand on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct StickyHeaderWithTabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
